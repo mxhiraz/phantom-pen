bae43e71b669deb53d04c158294f91940d1cbb1e
Tanmay <145962151+mxhiraz@users.noreply.github.com>
2025-08-21 02:14:44 +0530

    Refactor sso-callback page layout to use a div wrapper for improved structure and maintainability

--- app/auth/sso-callback/page.tsx
+++ app/auth/sso-callback/page.tsx
@@ -1,3 +1,4 @@
+import { Spinner } from "@/components/whisper-page/LoadingSection";
 import { AuthenticateWithRedirectCallback } from "@clerk/nextjs";
 
 export default function Page() {
@@ -5,9 +6,10 @@ export default function Page() {
   // or rendering the prebuilt <AuthenticateWithRedirectCallback/> component.
   // This is the final step in the custom OAuth flow.
   return (
-    <>
+    <div className=" min-h-screen flex items-center justify-center ">
+      <Spinner />
       <AuthenticateWithRedirectCallback />
       <div id="clerk-captcha"></div>
-    </>
+    </div>
   );
 }
