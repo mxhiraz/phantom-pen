0114d8ddfbb476cd00c89873ad526deddbb31889
Tanmay <145962151+mxhiraz@users.noreply.github.com>
2025-08-23 06:01:02 +0530

    refactor: simplify HeroSection component by removing unused elements and adjusting layout for clarity

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -178,25 +178,10 @@ export default function HeroSection() {
             </div>
 
             <div
-              aria-hidden
-              className="absolute inset-0 isolate hidden opacity-65 contain-strict lg:block"
-            >
-              <div className="w-140 h-320 -translate-y-87.5 absolute left-0 top-0 -rotate-45 rounded-full bg-[radial-gradient(68.54%_68.72%_at_55.02%_31.46%,hsla(0,0%,85%,.08)_0,hsla(0,0%,55%,.02)_50%,hsla(0,0%,45%,0)_80%)]" />
-              <div className="h-320 absolute left-0 top-0 w-60 -rotate-45 rounded-full bg-[radial-gradient(50%_50%_at_50%_50%,hsla(0,0%,85%,.06)_0,hsla(0,0%,45%,.02)_80%,transparent_100%)] [translate:5%_-50%]" />
-              <div className="h-320 -translate-y-87.5 absolute left-0 top-0 w-60 -rotate-45 bg-[radial-gradient(50%_50%_at_50%_50%,hsla(0,0%,85%,.04)_0,hsla(0,0%,45%,.02)_80%,transparent_100%)]" />
-
-              {/* <div
-                className=" absolute bottom-[-50] left-[-50]  blur-[50px] opacity-20 w-108 h-[300px] bg-gradient-to-r from-purple-700 to-purple-900 
+              className=" absolute top-[-50] right-[-50]  blur-[50px] opacity-20 w-108 h-[300px] bg-gradient-to-r from-purple-700 to-purple-900 
             animate-[blob_8s_infinite_ease-in-out] 
             rounded-[50%_40%_60%_50%_/_50%_60%_40%_50%]"
-              ></div> */}
-
-              <div
-                className=" absolute top-[-50] right-[-50]  blur-[50px] opacity-20 w-108 h-[300px] bg-gradient-to-r from-purple-700 to-purple-900 
-            animate-[blob_8s_infinite_ease-in-out] 
-            rounded-[50%_40%_60%_50%_/_50%_60%_40%_50%]"
-              ></div>
-            </div>
+            ></div>
           </div>
         </section>
 
